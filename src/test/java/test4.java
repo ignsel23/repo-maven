7af86d2decf7c9aed5a3d69315a1cfade3a2536b
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2024-12-16 11:29:29 +0100

    test junit5

--- src/test/java/test4.java
+++ src/test/java/test4.java
@@ -1,12 +1,12 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class test4 {
-
-	@Test
-	public void testSomma() {
-		fail("Not yet implemented");
-	}
-
-}
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//public class test4 {
+//
+//	@Test
+//	public void testSomma() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
